54b1f5038142bf7c6418eee41423e67bb5f38788
pedro <pedro.fausto@hotmail.com>
2016-11-01 15:27:12 -0300

    fix bug xml

--- lib/correios_api/resource/postage.rb
+++ lib/correios_api/resource/postage.rb
@@ -6,11 +6,11 @@ module CorreiosECT
       attribute :ticket, type: String
       attribute :invoice, type: String, default: '0000000'
       attribute :service, type: Integer
-      attribute :weight, type: String
-      attribute :type_object, type: Integer, default: 002
-      attribute :height, type: Integer, default: 0
-      attribute :width, type: Integer, default: 0
-      attribute :length, type: Integer, default: 0
+      attribute :weight, type: Integer
+      attribute :type_object, type: String, default: '002'
+      attribute :height, type: Integer, default: 20
+      attribute :width, type: Integer, default: 30
+      attribute :length, type: Integer, default: 38
       attribute :diameter, type: Integer, default: 0
       attribute :receiver, type: Object
 
