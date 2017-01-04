42d69db91c3b53706262ffa60b3d8ccfb0521e42
pedro <pedro.fausto@hotmail.com>
2017-01-04 09:00:47 -0300

    ..

--- examples/configuration.rb
+++ examples/configuration.rb
@@ -1,5 +1,14 @@
 
-
+CorreiosECT.configure do |config|
+  config.cnpj = '21075446000155'
+  config.user = 'pedro.fausto@hotmail.com'
+  config.password = 'an8bw4'
+  config.administrative_code = '16332113'
+  config.contract = '9912406220'
+  config.code_service = ''
+  config.card = '0072796901'
+  config.environment = :production
+end
   # config.cnpj=ENV['ECT_CNPJ']
   # config.user=ENV['ECT_USER']
   # config.password = ENV['ECT_PASSWORD']
