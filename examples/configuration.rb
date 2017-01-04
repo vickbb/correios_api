d66929b36551bd1a32c3d233ccf71776e052e4f4
pedro <pedro.fausto@hotmail.com>
2017-01-04 08:59:03 -0300

    remove config

--- examples/configuration.rb
+++ examples/configuration.rb
@@ -1,13 +1,4 @@
-CorreiosECT.configure do |config|
-  config.cnpj = '21075446000155'
-  config.user = 'pedro.fausto@hotmail.com'
-  config.password = 'an8bw4'
-  config.administrative_code = '16332113'
-  config.contract = '9912406220'
-  config.code_service = ''
-  config.card = '0072796901'
-  config.environment = :production
-end
+
 
   # config.cnpj=ENV['ECT_CNPJ']
   # config.user=ENV['ECT_USER']
@@ -17,12 +8,3 @@ end
   # config.code_service = ENV['ECT_CODE_SERVICE']
   # config.card = ENV['ECT_CARD']
   # config.environment = ENV['ECT_ENVIRONMENT'].to_sym
-
-  # config.cnpj = 21075446000155
-  # config.user = pedro.fausto@hotmail.com
-  # config.password = an8bw4
-  # config.administrative_code = 16332113
-  # config.contract = 9912406220
-  # config.code_service = ''
-  # config.card = 0072796901
-  # config.environment = :production
